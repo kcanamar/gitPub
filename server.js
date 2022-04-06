f80bfb1c78557b7a61278644bc222b81db9293f5
Kyle Canamar <canamar.kyle@gmail.com>
2022-04-05 23:34:13 -0700

    adds working drink and food menu

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001
 const drinks = require('./models/drinks.js')
+const foods = require('./models/food.js')
 
 //////////////////////
 // Declare Middleware
@@ -21,7 +22,13 @@ app.get("/drinks", (req, res) => {
     res.render('drinks_index.ejs', {allDrinks: drinks})
 })
 app.get("/drinks/:id", (req, res) => {
-    res.render('drinks_index.ejs', {allDrinks: drinks[req.params.id]})
+    res.render('drink_show.ejs', {drink: drinks[req.params.id]})
+})
+app.get("/foods", (req, res) => {
+    res.render('food_index.ejs', {allFood: foods})
+})
+app.get("/foods/:id", (req, res) => {
+    res.render('food_show.ejs', {food: foods[req.params.id]})
 })
 ///////////////////////////
 // Server Listener
